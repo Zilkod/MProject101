c05dab82c0cbfd769786ad769f368cf8969c6902
Zilkod <83935849+Zilkod@users.noreply.github.com>
2024-08-22 10:14:52 +0600

    .

---
+++ melinda-record-import-transformer-asteri-subjects-main/README.md
@@ -0,0 +1,7 @@
+# Asteri subjects record transformer for the Melinda record batch import system
+
+## License and copyright
+
+Copyright (c) 2020 **University Of Helsinki (The National Library Of Finland)**
+
+This project's source code is licensed under the terms of **GNU Affero General Public License Version 3** or any later version.
---
+++ melinda-record-import-transformer-asteri-subjects-main/src/config.js
@@ -0,0 +1,31 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* Asteri subjects record transformer for the Melinda record batch import system
+*
+* Copyright (C) 2019-2020 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-record-import-transformer-asteri-subjects
+*
+* melinda-record-import-transformer-asteri-subjects program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-record-import-transformer-asteri-subjects is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+//import {parseBoolean} from '@natlibfi/melinda-commons';
+//import {readEnvironmentVariable} from '@natlibfi/melinda-backend-commons';
+
